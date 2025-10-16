5130888564166cd326d2afdba16eb0516ff5d76f
SHIROKAMIQQ <migzguiang@gmail.com>
2025-10-16 19:25:23 +0800

    feat(saln-form/Children): add formData validation

--- saln-client/src/utils/assetTypes.ts
+++ saln-client/src/utils/assetTypes.ts
@@ -11,41 +11,148 @@ export enum AssetTypes {
 
 export const AssetFields = {
   [AssetTypes.UNMARRIED_CHILDREN]: [
-    {label: "Name", subtext: "Unmarried child below eighteen (18) years of age living in declarant's household", inputName: "Name"},
-    {label: "Date of Birth", inputName: "DoB"},
-    {label: "Age", inputName: "Age"}
+    {
+      label: "Name", 
+      subtext: "Unmarried child below eighteen (18) years of age living in declarant's household", 
+      inputName: "Name", 
+      type: "text"
+    },
+    {
+      label: "Date of Birth", 
+      subtext: "Must be less than 18 years ago",
+      inputName: "DoB", 
+      type: "date"
+    },
+    {
+      label: "Age", 
+      subtext: "Must match Date of Birth",
+      inputName: "Age", 
+      type:"number"
+    }
   ],
   [AssetTypes.REAL_PROPERTY]: [
-    {label: "Description", subtext: "(e.g. lot, house and lot, condominium, and improvements)", inputName: "Description"},
-    {label: "Kind", subtext: "(e.g. residential, commericial, industrial, agricultural and mixed used)", inputName: "Kind"},
-    {label: "Exact Location", inputName: "ExactLocation"},
-    {label: "Assessed Value", subtext: "(As found in the Tax Declaration of Real Property)", intputName: "AssessedValue"},
-    {label: "Current Fair Market Value", subtext: "(As found in the Tax Declaration of Real Property)", inputName: "CurrentFairMarketValue"},
-    {label: "Acquisition Year", inputName: "AcquisitionYear"},
-    {label: "Acquisition Mode", inputName: "AcquisitionMode"},
-    {label: "Acquisition Cost", inputName: "AcquisitionCost"}
+    {
+      label: "Description", 
+      subtext: "(e.g. lot, house and lot, condominium, and improvements)", 
+      inputName: "Description", 
+      type:"text"
+    },
+    {
+      label: "Kind", 
+      subtext: "(e.g. residential, commericial, industrial, agricultural and mixed used)", 
+      inputName: "Kind",
+      type:"text"
+    },
+    {
+      label: "Exact Location", 
+      inputName: "ExactLocation",
+      type: "text"
+    },
+    {
+      label: "Assessed Value", 
+      subtext: "(As found in the Tax Declaration of Real Property)", 
+      inputName: "AssessedValue",
+      type: "number"
+    },
+    {
+      label: "Current Fair Market Value", 
+      subtext: "(As found in the Tax Declaration of Real Property)", 
+      inputName: "CurrentFairMarketValue",
+      type: "number"
+    },
+    {
+      label: "Acquisition Year", 
+      inputName: "AcquisitionYear",
+      type: "number"
+    },
+    {
+      label: "Acquisition Mode", 
+      inputName: "AcquisitionMode",
+      type: "text"
+    },
+    {
+      label: "Acquisition Cost", 
+      inputName: "AcquisitionCost",
+      type: "number"
+    }
   ],
   [AssetTypes.PERSONAL_PROPERTY]: [
-    {label: "Description", inputName: "Description"},
-    {label: "Year Acquired", inputName: "YearAcquired"},
-    {label: "Acquisition Cost/Amount", inputName: "AcqusitionCost"}
+    {
+      label: "Description", 
+      inputName: "Description",
+      type: "text"
+    },
+    {
+      label: "Year Acquired",
+      inputName: "YearAcquired",
+      type: "number"
+    },
+    {
+      label: "Acquisition Cost/Amount",
+      inputName: "AcqusitionCost",
+      type: "number"
+    }
   ],
   [AssetTypes.LIABILITY]: [
-    {label: "Nature", inputName: "Nature"},
-    {label: "Name of Creditors", inputName: "Creditors"},
-    {label: "Outstanding Balance", inputName: "OutstandingBalance"}
+    {
+      label: "Nature", 
+      inputName: "Nature",
+      type: "text"
+    },
+    {
+      label: "Name of Creditors", 
+      inputName: "Creditors",
+      type: "text"
+    },
+    {
+      label: "Outstanding Balance", 
+      inputName: "OutstandingBalance",
+      type: "number"
+    }
   ],
   [AssetTypes.CONNECTION]: [
-    {label: "Name of Entity/Business Enterprise", inputName: "Name"},
-    {label: "Business Address", inputName: "BusinessAddress"},
-    {label: "Nature of Business Interest & / or Financial Connection", inputName: "Nature"},
-    {label: "Date of Acquisition of Interest or Connection", inputName: "DateOfAcquisition"}
+    {
+      label: "Name of Entity/Business Enterprise", 
+      inputName: "Name",
+      type: "text"
+    },
+    {
+      label: "Business Address", 
+      inputName: "BusinessAddress",
+      type: "text"
+    },
+    {
+      label: "Nature of Business Interest & / or Financial Connection", 
+      inputName: "Nature",
+      type: "text"
+    },
+    {
+      label: "Date of Acquisition of Interest or Connection", 
+      inputName: "DateOfAcquisition",
+      type: "number"
+    }
   ],
   [AssetTypes.RELATIVE]: [
-    {label: "Name of Relative", inputName: "Name"},
-    {label: "Relationship", inputName: "Relationship"},
-    {label: "Position", inputName: "Position"},
-    {label: "Name of Agency/Office and Address", inputName: "Agency"}
+    {
+      label: "Name of Relative", 
+      inputName: "Name",
+      type: "text"
+    },
+    {
+      label: "Relationship", 
+      inputName: "Relationship",
+      type: "text"
+    },
+    {
+      label: "Position", 
+      inputName: "Position",
+      type: "text"
+    },
+    {
+      label: "Name of Agency/Office and Address", 
+      inputName: "Agency",
+      type: "text"
+    }
   ]
 };
 
