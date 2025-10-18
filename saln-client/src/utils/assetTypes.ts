fbb66c67d79ec851787a28004a3e9971694d87bb
SHIROKAMIQQ <migzguiang@gmail.com>
2025-10-18 08:08:31 +0800

    chore: remove type attribute from AssetFields

--- saln-client/src/utils/assetTypes.ts
+++ saln-client/src/utils/assetTypes.ts
@@ -15,19 +15,16 @@ export const AssetFields = {
       label: "Name", 
       subtext: "Unmarried child below eighteen (18) years of age living in declarant's household", 
       inputName: "Name", 
-      type: "text"
     },
     {
       label: "Date of Birth", 
       subtext: "Must be less than 18 years ago",
       inputName: "DoB", 
-      type: "date"
     },
     {
       label: "Age", 
       subtext: "Must match Date of Birth",
       inputName: "Age", 
-      type:"number"
     }
   ],
   [AssetTypes.REAL_PROPERTY]: [
@@ -35,125 +32,103 @@ export const AssetFields = {
       label: "Description", 
       subtext: "(e.g. lot, house and lot, condominium, and improvements)", 
       inputName: "Description", 
-      type:"text"
     },
     {
       label: "Kind", 
       subtext: "(e.g. residential, commericial, industrial, agricultural and mixed used)", 
       inputName: "Kind",
-      type:"text"
     },
     {
       label: "Exact Location", 
       inputName: "ExactLocation",
-      type: "text"
     },
     {
       label: "Assessed Value", 
       subtext: "(As found in the Tax Declaration of Real Property) | At most 2 decimal places", 
       inputName: "AssessedValue",
-      type: "text"
     },
     {
       label: "Current Fair Market Value", 
       subtext: "(As found in the Tax Declaration of Real Property) | At most 2 decimal places", 
       inputName: "CurrentFairMarketValue",
-      type: "text"
     },
     {
       label: "Acquisition Year", 
       subtext: "YYYY",
       inputName: "AcquisitionYear",
-      type: "text"
     },
     {
       label: "Acquisition Mode", 
       inputName: "AcquisitionMode",
-      type: "text"
     },
     {
       label: "Acquisition Cost", 
       subtext: "At most 2 decimal places",
       inputName: "AcquisitionCost",
-      type: "number"
     }
   ],
   [AssetTypes.PERSONAL_PROPERTY]: [
     {
       label: "Description", 
       inputName: "Description",
-      type: "text"
     },
     {
       label: "Year Acquired",
       inputName: "YearAcquired",
-      type: "number"
     },
     {
       label: "Acquisition Cost/Amount",
       inputName: "AcqusitionCost",
-      type: "number"
     }
   ],
   [AssetTypes.LIABILITY]: [
     {
       label: "Nature", 
       inputName: "Nature",
-      type: "text"
     },
     {
       label: "Name of Creditors", 
       inputName: "Creditors",
-      type: "text"
     },
     {
       label: "Outstanding Balance", 
       inputName: "OutstandingBalance",
-      type: "number"
     }
   ],
   [AssetTypes.CONNECTION]: [
     {
       label: "Name of Entity/Business Enterprise", 
       inputName: "Name",
-      type: "text"
     },
     {
       label: "Business Address", 
       inputName: "BusinessAddress",
-      type: "text"
     },
     {
       label: "Nature of Business Interest & / or Financial Connection", 
       inputName: "Nature",
-      type: "text"
     },
     {
       label: "Date of Acquisition of Interest or Connection", 
       inputName: "DateOfAcquisition",
-      type: "number"
     }
   ],
   [AssetTypes.RELATIVE]: [
     {
       label: "Name of Relative", 
       inputName: "Name",
-      type: "text"
     },
     {
       label: "Relationship", 
       inputName: "Relationship",
-      type: "text"
     },
     {
       label: "Position", 
       inputName: "Position",
-      type: "text"
     },
     {
       label: "Name of Agency/Office and Address", 
       inputName: "Agency",
-      type: "text"
     }
   ]
 };
