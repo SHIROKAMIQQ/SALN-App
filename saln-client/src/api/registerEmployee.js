a87711f314d7c0d3301c1b7b3022f73fd5d0bd0f
SHIROKAMIQQ <migzguiang@gmail.com>
2025-10-22 00:37:55 +0800

    feat: register-employee API endpoint

---
+++ saln-client/src/api/registerEmployee.js
@@ -0,0 +1,43 @@
+import { v4 as uuidv4 } from "uuid";
+import { API_BASE_URL } from "./config";
+
+/**
+ * Register a new employee
+ * @param {string} email
+ */
+export async function registerEmployee(email) {
+  console.log("API call to registerEmployee");
+  
+  if (!email) throw new Error("Email is required");
+
+  // Generate employeeID UUID
+  const employeeID = uuidv4();
+
+  // Generate encryption key for this employee
+  const cryptoKey = await crypto.subtle.generateKey(
+    {name: "AES-GCM", length: 256},
+    true,
+    ["encrypt", "decrypt"]
+  );
+
+  // Export the key to a base64 string so it can be sent and stored into the database
+  const exportedKey = await crypto.subtle.exportKey("raw", cryptoKey);
+  const exportedKeyBase64 = btoa(String.fromCharCode(...new Uint8Array(exportedKey)));
+
+  // Send to server
+  const response = await fetch(`${API_BASE_URL}/register-employee`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      employeeID: employeeID,
+      email: email,
+      encryption_key: exportedKeyBase64 
+    }),
+  });
+
+  const data = await response.json();
+  console.log("Server resposne:", data);
+  return data;
+}
