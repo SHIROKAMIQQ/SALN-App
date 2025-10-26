875dc2f0ed8be163be2c208685f765ec980c005a
Lucas Miguel V. Agcaoili <lvagcaoili@up.edu.ph>
2025-10-26 22:53:32 +0800

    add routing to dashboard

--- saln-client/.marko-run/routes.d.ts
+++ saln-client/.marko-run/routes.d.ts
@@ -11,6 +11,7 @@ declare module "@marko/run" {
 	interface AppData extends Run.DefineApp<{
 		routes: {
 			"/": { verb: "get"; meta: typeof import("../src/routes/+meta.json"); };
+			"/dashboard": { verb: "get"; };
 			"/login": { verb: "get"; };
 			"/otp-verification": { verb: "get"; };
 			"/registration": { verb: "get"; };
@@ -24,6 +25,17 @@ declare module "../src/routes/+page.marko" {
   namespace MarkoRun {
     export { NotHandled, NotMatched, GetPaths, PostPaths, GetablePath, GetableHref, PostablePath, PostableHref, Platform };
     export type Route = Run.Routes["/"];
+    export type Context = Run.MultiRouteContext<Route> & Marko.Global;
+    export type Handler = Run.HandlerLike<Route>;
+    /** @deprecated use `((context, next) => { ... }) satisfies MarkoRun.Handler` instead */
+    export const route: Run.HandlerTypeFn<Route>;
+  }
+}
+
+declare module "../src/routes/dashboard/+page.marko" {
+  namespace MarkoRun {
+    export { NotHandled, NotMatched, GetPaths, PostPaths, GetablePath, GetableHref, PostablePath, PostableHref, Platform };
+    export type Route = Run.Routes["/dashboard"];
     export type Context = Run.MultiRouteContext<Route> & Marko.Global;
     export type Handler = Run.HandlerLike<Route>;
     /** @deprecated use `((context, next) => { ... }) satisfies MarkoRun.Handler` instead */
@@ -90,7 +102,7 @@ declare module "../src/routes/+layout.marko" {
   export interface Input extends Run.LayoutInput<typeof import("../src/routes/+layout.marko")> {}
   namespace MarkoRun {
     export { NotHandled, NotMatched, GetPaths, PostPaths, GetablePath, GetableHref, PostablePath, PostableHref, Platform };
-    export type Route = Run.Routes["/" | "/login" | "/otp-verification" | "/registration" | "/saln-form" | "/uploadJSON"];
+    export type Route = Run.Routes["/" | "/dashboard" | "/login" | "/otp-verification" | "/registration" | "/saln-form" | "/uploadJSON"];
     export type Context = Run.MultiRouteContext<Route> & Marko.Global;
     export type Handler = Run.HandlerLike<Route>;
     /** @deprecated use `((context, next) => { ... }) satisfies MarkoRun.Handler` instead */
