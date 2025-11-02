bb96a98b6a2179f25e594df29ae5804c0850fd60
Lucas Miguel V. Agcaoili <lvagcaoili@up.edu.ph>
2025-11-02 16:37:31 +0800

    fix(OTP Verification): Re-add OTP Verification

--- saln-server/routes/api.php
+++ saln-server/routes/api.php
@@ -1,11 +1,9 @@
 <?php 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
-use App\Http\Services\OtpService;
-
 
 Route::post('/register-employee', [EmployeeController::class, 'register']);
 
 Route::post('/login-employee', [EmployeeController::class, 'login']);
 
-Route::post('/verify-otp', [OtpService::class, 'verifyOTP']);
+Route::post('/verify-otp', [EmployeeController::class, 'verifyOTP']);
