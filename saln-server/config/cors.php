c59b103a10e97280d382a5daedfde2c26fec89ca
SHIROKAMIQQ <migzguiang@gmail.com>
2026-02-11 07:18:30 +0800

    build: add lamig-saln domain to cors

--- saln-server/config/cors.php
+++ saln-server/config/cors.php
@@ -1,9 +1,27 @@
 <?php
 
+// FOR DEV:
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000', 
+        'http://127.0.0.1:3000'
+    ],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
 ];
+
+// FOR PROD:
+// return [
+//     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+//     'allowed_methods' => ['*'],
+//     'allowed_origins' => [
+//         'http://localhost:3000', 
+//         'http://127.0.0.1:3000',
+//         'https://lamig-saln.online',
+//         'https://www.lamig-saln.online'
+//     ],
+//     'allowed_headers' => ['*'],
+//     'supports_credentials' => true,
+// ];
