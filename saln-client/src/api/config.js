7344d9577d21374fb278927c44fa1fc60065027a
SHIROKAMIQQ <migzguiang@gmail.com>
2026-04-29 17:21:39 +0800

    chore: use PROD URL for API config

--- saln-client/src/api/config.js
+++ saln-client/src/api/config.js
@@ -1,5 +1,5 @@
 // FOR DEV:
-export const API_BASE_URL = "http://127.0.0.1:8000/api";
+// export const API_BASE_URL = "http://127.0.0.1:8000/api";
 
 // FOR PROD:
-// export const API_BASE_URL = "https://lamig-saln.online/api";
+export const API_BASE_URL = "https://lamig-saln.online/api";
